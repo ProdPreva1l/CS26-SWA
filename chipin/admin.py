1cc0ed34ab0947d5558b443906afcbfdcf53f891
Preva1l <prod.preva1l@gmail.com>
2026-02-11 16:23:38 +1100

    feat: join requests & request voting

--- chipin/admin.py
+++ chipin/admin.py
@@ -4,9 +4,3 @@ from .models import Group, Invite
 @admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
     list_display = ("name", "admin")
-
-@admin.register(Invite)
-class InviteAdmin(admin.ModelAdmin):
-    list_display = ("group", "invited_user", "invited_by", "accepted", "expires_at", "created_at")
-    list_filter = ("accepted", "group")
-    search_fields = ("group__name", "invited_user__username", "invited_by__username")
--- core/settings.py
+++ core/settings.py
@@ -76,13 +76,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'chipin.context_processors.user_profile',
             ],
         },
     },
 ]
 
 WSGI_APPLICATION = 'core.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
@@ -93,7 +93,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
@@ -113,7 +112,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
@@ -124,7 +122,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
